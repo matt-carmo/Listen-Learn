5fa5ba37cda2c374ab85c617a0c9567c7c546ae9
matt-carmo <matheus2018i@gmail.com>
2025-04-25 13:30:28 -0300

    force deploy

--- front-end/phrase-play/app/page.tsx
+++ front-end/phrase-play/app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
   const gradedReaders: GradedReader[] = await data.json();
   return (
     <>
-      <Header title="Listen & Learn" />     
+      <Header title="Listen & Learn"/>     
       <div className="grid grid-rows-[20px_1fr_20px]   p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col items-center w-full max-w-2xl mx-auto ">
           <div className="flex w-full">
